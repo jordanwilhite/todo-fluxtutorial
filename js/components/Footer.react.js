e2835722f4c5ebd054bec0c620886787ed5440a6
Jordan Wilhite <jordanwilhite@gmail.com>
2015-08-21 07:09:48 -0500

    first commit

---
+++ js/components/Footer.react.js
@@ -0,0 +1,60 @@
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+var TodoActions = require ('../actions/TodoActions');
+
+var Footer = React.createClass ({
+
+  propTypes: {
+    allTodos: ReactPropTypes.object.isRequired
+  },
+
+  render: function() {
+    var allTodos = this.props.allTodos;
+    var total = Object.keys(allTodos).length;
+
+    if (total === 0) {
+      return null;
+    }
+
+    var complete = 0;
+    for (var key in allTodos) {
+      if (allTodos[key].complete) {
+        completed++;
+      }
+    }
+
+    var itemsLeft = total - completed;
+    var itemsLeftPhrase = itemsLeft === 1 ? ' item ':
+    ' item ';
+    itemsLeftPhrase += 'left';
+
+    var clearCompletedButton;
+    if (completed) {
+      clearCompletedButton =
+      <button
+        id="clear-completed"
+        onClick={this._onClearCompletedClick}>
+        Clear Completed ({completed})
+      </button>;
+    }
+
+    return(
+      <footer id = "footer">
+        <span id ="todo-count">
+          <strong>
+            {itemsLeft}
+          </strong>
+          {itemsLeftPhrase}
+        </span>
+        {clearCompletedButton}
+      </footer>
+    );
+  },
+
+  _onClearCompletedClick: function() {
+    TodoActions.destroyCompleted();
+  }
+
+});
+
+module.exports = Footer;  
